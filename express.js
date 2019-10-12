aeec88e7b4b7b57039f8ae95977ac700dbb885f4
linxiaowu66 <linguang661990@126.com>
2019-10-12 16:24:53 +0800

    feat(express): add express demo

--- express.js
+++ express.js
@@ -2,4 +2,44 @@ const express = require('express')
 
 const app = express()
 
-app.listen()
+const sleep = (mseconds) => new Promise((resolve) => setTimeout(resolve, mseconds))
+
+app.use(async (req, res, next) => {
+  console.log('I am the first middleware')
+  next()
+  console.log('first middleware end calling')
+})
+app.use((req, res, next) => {
+  console.log('I am the second middleware')
+  next()
+  console.log('second middleware end calling')
+})
+
+app.get('/api/test1', async(req, res, next) => {
+  console.log('I am the router middleware => /api/test1')
+  await sleep(2000)
+  res.status(200).send('hello')
+})
+app.get('/api/test2', async(req, res, next) => {
+  console.log('I am the router middleware => /api/test2')
+  res.status(200).send('world')
+})
+app.get('/api/testerror', (req, res, next) => {
+  console.log('I am the router middleware => /api/testerror')
+  throw new Error('I am error.')
+})
+
+app.use(async(err, req, res, next) => {
+  if (err) {
+    console.log('last middleware catch error', err)
+    res.status(500).send('server Error')
+    return
+  }
+  console.log('I am the last middleware')
+  await sleep(2000)
+  next()
+  console.log('last middleware end calling')
+})
+
+app.listen(3001)
+console.log('server listening at port 3001')
---
+++ express_mysterious.js
@@ -0,0 +1,36 @@
+const express = require('express')
+
+const app = express()
+
+const sleep = (mseconds) => new Promise((resolve) => setTimeout(() => {
+  // 为什么这里的打印是最后？
+  console.log('sleep timeout...')
+  resolve()
+}, mseconds))
+
+app.use(async (req, res, next) => {
+  console.log('I am the first middleware')
+  const startTime = Date.now()
+  console.log(`================ start ${req.method} ${req.url}`, { query: req.query, body: req.body });
+  res.on('finish', () => {
+    const cost = Date.now() - startTime
+    console.log(`================ end ${req.method} ${req.url} ${res.statusCode} - ${cost} ms`)
+  })
+  next()
+  // const cost = Date.now() - startTime
+  // console.log(`================ end ${req.method} ${req.url} ${res.statusCode} - ${cost} ms`)
+})
+app.use((req, res, next) => {
+  console.log('I am the second middleware')
+  next()
+  console.log('second middleware end calling')
+})
+
+app.get('/api/test1', async(req, res, next) => {
+  console.log('I am the router middleware => /api/test1')
+  await sleep(2000)
+  res.status(200).send('hello')
+})
+
+app.listen(3001)
+console.log('server listening at port 3001')
